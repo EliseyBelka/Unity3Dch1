d390dd1bec9273c29d0a38b39be9543c50a0bf3a
EliseyBelka <46591130+EliseyBelka@users.noreply.github.com>
2019-12-15 03:12:08 +0700

    Add files via upload

---
+++ MyScriptEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(TestScript))]
+public class MyScriptЕditor : Editor
+{
+    bool _isPressButtonOk;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        TestScript testTarget = (TestScript)target;
+
+        var isPressButton = GUILayout.Button("Создание объектов по кнопке", EditorStyles.miniButton);
+
+        if (isPressButton)
+        {
+            testTarget.CreateObj();
+            _isPressButtonOk = true;
+        }
+
+        if (_isPressButtonOk)
+        {
+            EditorGUILayout.HelpBox("Вы нажали на кнопку", MessageType.Warning);
+        }
+    }
+}
---
+++ TestScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private int count = 1;
+    [SerializeField] private int offset = 1;
+    [SerializeField] private GameObject obj;
+    [SerializeField] private Axis axis;
+
+    enum Axis
+    {
+        x,y,z
+    }
+   
+    void Start()
+    {
+        CreateObj();
+    }
+
+    public void CreateObj()
+    {
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        if (axis == Axis.x)
+        {
+            x = offset;
+        }
+        else
+         if (axis == Axis.y)
+        {
+            y = offset;
+        }
+        else
+         if (axis == Axis.z)
+        {
+            z = offset;
+        }
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(obj,new Vector3(x*i,y*i,z*i),Quaternion.identity);
+        }
+    }
+}
